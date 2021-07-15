205008648321689bd48e1e8bbb6f7f7195e6fb4c
ralph emmason <remmason@yahoo.com>
2021-07-15 01:02:29 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+
+
+
+
+## Credits
+##### Free CSS 
+<a href="https://www.free-css.com/assets/files/free-css-templates/preview/page234/interact/">Free-CSS.com </a>
+
+##### Ralph Emmason
---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { useState, useEffect } from 'react'
+import { Header } from './components/header'
+import { Contact } from './components/contact'
+import JsonData from './data/data.json'
+import SmoothScroll from 'smooth-scroll'
+
+export const scroll = new SmoothScroll('a[href*="#"]', {
+  speed: 1000,
+  speedAsDuration: true,
+})
+
+const App = () => {
+  const [landingPageData, setLandingPageData] = useState({})
+  useEffect(() => {
+    setLandingPageData(JsonData)
+  }, [])
+
+  return (
+    <div>
+      <Header data={landingPageData.Header} />
+      <Contact data={landingPageData.Contact} />
+    </div>
+  )
+}
+
+export default App
